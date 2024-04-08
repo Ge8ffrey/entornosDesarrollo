edfe0615fb75b42f86fbd98b7fd76bbd7b275ee7
geoff <geoff@192.168.1.14>
2024-04-08 21:25:38 +0200

    Esta es la prueba de Commit and Push

--- Proyecto1/src/Paquete1/Principal.java
+++ Proyecto1/src/Paquete1/Principal.java
@@ -3,7 +3,7 @@ package Paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Estamos haciendo control con versiones con Git");
+		System.out.println("Esta es la segunda prueba");
 
 	}
 
